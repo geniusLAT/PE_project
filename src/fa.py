80d6db5b74750c758316f0630de4f8f7e14a8469
Alexandr <149713750+Ashav1309@users.noreply.github.com>
2024-03-03 21:27:10 +0500

    Update fa.py

--- src/fa.py
+++ src/fa.py
@@ -1,16 +1,16 @@
 from fastapi import FastAPI
-
+import model
 
 app = FastAPI()
-import model
+
 
 def process(input_text):
     try:
-        payload =str(model.analyse(input_text)[0])
-        print("model payload: "+payload)
+        payload = str(model.analyse(input_text)[0])
+        print("model payload: " + payload)
         return payload
     except Exception as e:
-        print("It is error on line 12:" +str(e))
+        print("It is error on line 12:" + str(e))
         print("Error inside the model.py")
         return "model.py error"
 
@@ -21,9 +21,8 @@ async def process_text(text: str):
         result = process(text)
         print("Text prosessed normally.")
         return {"result": result}
-    
+
     except Exception as e:
-        print("It is error on line 25:" +str(e))
+        print("It is error on line 25:" + str(e))
         print("Error inside the processing")
         return {"result": "None"}
-
