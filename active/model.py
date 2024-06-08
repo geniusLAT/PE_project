96295e14deb8862c179dedf9de102fd496827104
Moshwen <149946493+Moshwen@users.noreply.github.com>
2024-06-08 11:24:06 +0300

    Update model.py

--- active/model.py
+++ active/model.py
@@ -4,12 +4,12 @@ clf = pipeline(
     task='sentiment-analysis',
     model='SkolkovoInstitute/russian_toxicity_classifier')
 
-text = ['У нас в есть убунты и текникал превью.','Как минимум два малолетних дегенерата в треде, мда.','иди на хер']
+text = ['У нас в есть убунты и текникал превью.', 'Как минимум два малолетних дегенерата в треде, мда.', 'иди на хер']
 
 
 def analyse(text):
-    text=[text]
-    result=clf(text)
+    text= [text]
+    result= clf(text)
     print(result)
     return result
 
