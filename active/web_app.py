4e9b55006acd04a561644ec70efc87d870eb9150
Moshwen <149946493+Moshwen@users.noreply.github.com>
2024-06-08 11:25:32 +0300

    Update web_app.py

--- active/web_app.py
+++ active/web_app.py
@@ -12,6 +12,7 @@ output_text = st.empty()
 def process_text(input_text):
     return str(model.analyse(input_text)[0])
 
+
 # Обрабатываем введенный текст и выводим результат
 if st.button('Показать результат'):
     result = process_text(input_text)
