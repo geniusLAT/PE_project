19ca7b8a6de03b22c0a8d1130e2e2e24c7ae9569
Moshwen <149946493+Moshwen@users.noreply.github.com>
2024-06-08 11:50:48 +0300

    Update model.py

--- active/model.py
+++ active/model.py
@@ -1,12 +1,10 @@
 from transformers import pipeline
 
 clf = pipeline(
-    task='sentiment-analysis', 
+    task='sentiment-analysis',
     model='SkolkovoInstitute/russian_toxicity_classifier')
 
-text = ['У нас в есть убунты и текникал превью.',
-    	'Как минимум два малолетних дегенерата в треде, мда.',
-        'иди на хер']
+text = ['У нас в есть убунты и текникал превью.', 'Как минимум два малолетних дегенерата в треде, мда.', 'иди на хер']
 
 
 def analyse(text):
