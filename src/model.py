06320e54e0d97e195af73c01434c1d1b6ba87080
Ashav1309 <ashav1309@gmail.com>
2024-06-23 21:57:42 +0500

    commit

--- src/model.py
+++ src/model.py
@@ -1,20 +1,22 @@
 from transformers import pipeline
 
+# Создание классификатора для анализа тональности текста
 clf = pipeline(
     task='sentiment-analysis',
     model='SkolkovoInstitute/russian_toxicity_classifier')
 
+# Примеры текстов для анализа
 text = ['У нас в есть убунты и текникал превью.',
         'Как минимум два малолетних дегенерата в треде, мда.',
         'иди на хер']
 
-
+# Функция для анализа текста
 def analyse(text):
     text = [text]
     result = clf(text)
     # print(result)
     return result
 
-
+# Проверка функции при запуске скрипта
 if __name__ == '__main__':
     print(analyse("Кто любит жаб? Они крутые."))
