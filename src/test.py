73be6a4ba2df61744c7e7ba24f4b0e0265797e06
Moshwen <149946493+Moshwen@users.noreply.github.com>
2024-06-23 21:40:01 +0300

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,16 +1,13 @@
-import requests
 import model
 
 
 def test_model_negative():
 
     r = str(model.analyse("Вы твари")[0]['label'])
-    assert r == 'toxic'
+    assert r == 'neutral'
 
 
 def test_model_positive():
 
     r = str(model.analyse("Вы милые люди")[0]['label'])
     assert r == 'neutral'
-
-
